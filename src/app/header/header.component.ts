f350e544feab5aca63236e83327d3a11846caeaf
Jim Macur <jim.macur@gmail.com>
2025-01-17 18:35:13 -0700

    add thinking behind question component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { QuestionComponent } from "../question/question.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [QuestionComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isGameStarted = false;
+  
   startGame() {
+    this.isGameStarted = true;
     console.log('Game started');
   }
 }
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './question.component.css'
 })
 export class QuestionComponent {
+  // this is where the questions and answers will be stored
+  // i want objects for individual grade questions and answers like 5 for 1st, 5 for 2nd, 5 for 3rd, 5 for 4th, 5 for 5th
+  // The final 6th grade question will come from an API call
+  //
+  // 1st question will be a random question from 1st grade
+  // 2nd question will be a random question from 2nd grade etc through 5th grade
+  // 6th question will be from the API
+
 
 }
