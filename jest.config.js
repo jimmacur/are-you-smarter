05ac8473c6a01e8517d4da3f9e26b38047dd338e
Jim Macur <jim.macur@gmail.com>
2025-01-19 14:23:57 -0700

    add jest setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
+};
