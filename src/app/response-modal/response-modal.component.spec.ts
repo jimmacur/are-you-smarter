57eba39737e7bf692bfd1682ee852ec78a233ba8
Jim Macur <jim.macur@gmail.com>
2025-01-20 11:01:52 -0700

    update response modal to use routermodule

--- src/app/response-modal/response-modal.component.spec.ts
+++ src/app/response-modal/response-modal.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter, Router } from '@angular/router';
 import { ResponseModalComponent } from './response-modal.component';
+import { Location } from '@angular/common';
 
 describe('ResponseModalComponent', () => {
   let component: ResponseModalComponent;
   let fixture: ComponentFixture<ResponseModalComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
--- src/app/response-modal/response-modal.component.ts
+++ src/app/response-modal/response-modal.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-response-modal',
   templateUrl: './response-modal.component.html',
   styleUrls: ['./response-modal.component.css'],
-  imports: [NgIf]
+  imports: [NgIf, RouterModule]
 })
 export class ResponseModalComponent {
   @Input() showModal = false;
@@ -17,10 +17,7 @@ export class ResponseModalComponent {
   @Output() onNext = new EventEmitter<void>();
   @Output() onReset = new EventEmitter<void>();
 
-  constructor(private router: Router) {}
-
   close() {
-    this.router.navigate(['/home']);
     this.onClose.emit();
   }
 
