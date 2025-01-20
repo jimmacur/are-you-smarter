9ccb0224a7ed5f0c787d5787ad5c870a89421811
Jim Macur <jim.macur@gmail.com>
2025-01-20 10:00:21 -0700

    add testing for routing within home component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { Location } from '@angular/common';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideRouter([])]
+      providers: [provideRouter([
+        { path: 'game', component: HomeComponent },
+        { path: 'about', component: HomeComponent }
+      ])]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+
+    router.initialNavigation();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to "game" when the Play button is clicked', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const playButton = compiled.querySelector('button[routerLink="/game"]') as HTMLElement;
+
+    playButton.click();
+    await fixture.whenStable();
+
+    expect(location.path()).toBe('/game');
+  });
+
+  it('should navigate to "about" when the About button is clicked', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const aboutButton = compiled.querySelector('button[routerLink="/about"]') as HTMLElement;
+
+    aboutButton.click();
+    await fixture.whenStable();
+
+    expect(location.path()).toBe('/about');
   });
 
   it('should have a title "Are You Smarter Than a 6th Grader?"', () => {
@@ -28,4 +59,15 @@ describe('HomeComponent', () => {
     expect(titleElement?.textContent).toContain('Are You Smarter Than a 6th Grader?');
   });
 
+  it('should have a button to start the game', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const playButton = compiled.querySelector('button[routerLink="/game"]');
+    expect(playButton).toBeTruthy();
+  });
+
+  it('should have a button to navigate to about page', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const aboutButton = compiled.querySelector('button[routerLink="/about"]');
+    expect(aboutButton).toBeTruthy();
+  });
 });
